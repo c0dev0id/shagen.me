c9edcc612f983cf6afa01af8d8096458d130457c
c0dev0id <sh+git@codevoid.de>
2025-01-08 11:36:46 +0100

    Content Update

--- scripts/notifyupdate.sh
+++ scripts/notifyupdate.sh
@@ -3,10 +3,18 @@ cd /home/sdk/blog
 inotifywait -m -r . -e modify -e move -e create -e delete \
     | while read event
 do
-    echo "Processeing event: $event"
+    echo "Got event: $event"
     case "$event" in
-        */.git/*) echo "Ignoring change in /.git/" ;;
-        */public/*) echo "Ignoring change in /public/" ;;
-             *) hugo && make update || true ;;
+        */.git/*) echo "-> ignored" ;;
+      */public/*) echo "-> ignored" ;;
+               *) echo -n "-> testbuild: " 
+                  if hugo --quiet=true
+                   then
+                       echo "ok"
+                       echo "-> deploy"
+                       make update
+                   else
+                       echo "not ok, won't deploy"
+                   fi
     esac
 done
